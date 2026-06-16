b1adbd77ba1b4ddab75c11f309f5093644492d95
vikram2547 <mvikram104@gmail.com>
2026-06-16 11:14:42 +0530

    build run done

--- dist/assets/index-vmaFIJcj.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CMACEz2b.js";import{C as p}from"./chips.esm-cMzQfzul.js";const C=({value:o,onChange:n,placeholder:r="Type and enter",className:s})=>e.jsx(p,{className:s,value:o,onChange:t=>n(t.value),placeholder:r});export{C};
